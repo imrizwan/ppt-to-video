e082d6e4996d0b4cdef0f44390b672ee39bccdb5
imrizwan <mrizwan@digital-dividend.com>
2020-08-01 00:35:51 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+## Installation
+
+ppt-to-video depends on 'libreoffice':
+
+```
+  $ apt-get install libreoffice
+```
+
+ppt-to-video depends on 'ffmpeg':
+
+
+```
+  $ apt-get install ffmpeg
+```
+
+ppt-to-video depends on 'graphicsmagick':
+
+```
+  $ apt-get install graphicsmagick
+```
+
+
+ppt-to-video depends on 'curl':
+
+```
+  $ apt-get install curl
+```
+
+To install use npm:
+
+```
+  $ npm install --save ppt-to-video
+```
+
+## Usage
+e.g.
+```javascript
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+const multer = require('multer')
+const {p2vConverter} = require("ppt-to-video")
+const asyncHandler = require("express-async-handler");
+const uploads = multer({ dest: 'public/' })
+const app = express()
+
+app.post('/', uploads.single('file'), asyncHandler(async (req, res) => {
+  const file = req.file
+  const filename = file.filename
+  const filepath = file.path
+  const videoOptions = {
+    fps: 25,
+    loop: 5, // seconds
+    transition: true,
+    transitionDuration: 1, // seconds
+    videoBitrate: 1024,
+    videoCodec: 'libx264',
+    size: '640x?',
+    format: 'mp4',
+    pixelFormat: 'yuv420p'
+  }
+  const pdf2PicOptions = {
+    density: 100,           // output pixels per inch
+    format: "png",          // output file format
+    size: "600x600"         // output size in pixels
+  }
+  const videoPath = path.join(__dirname)
+  const thumbnail = true;
+  const thumbnailPath = path.join(__dirname)
+  await p2vConverter(filepath, filename, videoOptions, pdf2PicOptions, videoPath, thumbnail, thumbnailPath).then(output => console.log(`video saved at: ${output}`))
+  fs.unlinkSync(filepath)
+  return res.json("Video Converted")
+}));
+
+app.listen(8080, function () {
+  console.log('app listening on port 8080!');
+});
+
+```
---
+++ index.js
@@ -0,0 +1,112 @@
+const PDF2Pic = require("pdf2pic")
+const videoshow = require("videoshow")
+const _ = require("lodash")
+const path = require("path")
+const fs = require("fs")
+const gm = require("gm")
+const libre = require("libreoffice-convert")
+
+function generateThumbnail(outputPath, filename, thumbnailPath) {
+  return new Promise((resolve, reject) => {
+    gm(outputPath) // The name of your pdf
+      .setFormat("jpg")
+      .resize(200) // Resize to fixed 200px width, maintaining aspect ratio
+      .quality(75) // Quality from 0 to 100
+      .write(`${thumbnailPath}/${filename}.jpg`, function (error) {
+        // Callback function executed when finished
+        if (!error) {
+          resolve("Thumbnail generated")
+        } else {
+          reject(`error from gm, ${error}`)
+        }
+      })
+  })
+}
+function libreConverter(readFile, outputPath) {
+  return new Promise((resolve, reject) => {
+    console.log('Conversion to PDF started');
+    libre.convert(readFile, "pdf", undefined, (err, done) => {
+      if (err) {
+        reject(`Libre error: ${err}`)
+      } else {
+        fs.writeFileSync(outputPath, done);
+        resolve("Converted to PDF");
+      }
+    });
+
+  })
+}
+function pdf2picConverter(outputPath, pdf2PicOptions) {
+  return new Promise((resolve, reject) => {
+    const pdf2pic = new PDF2Pic(pdf2PicOptions)
+
+    pdf2pic.convertBulk(outputPath, -1).then(() => { //resolve
+      resolve("PDF converted to png")
+    }).catch((err) => {
+      reject("error from PDF2Pic", err)
+    })
+  }
+  )
+}
+
+
+function converttoVideo(exportPath, filename, videoOptions, videoPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(exportPath, (err, data) => {
+      if (!err && !_.isEmpty(data)) {
+        const imagePaths = data.map(item => `${exportPath}/${item}`)
+        videoshow(imagePaths, videoOptions)
+          .save(`${videoPath}/${filename}.mp4`)
+          .on('start', function (command) {
+            console.log('videoshow process started:', command)
+          })
+          .on('error', function (err, stdout, stderr) {
+            reject('videoshow Error:', err)
+            reject('videoshow stderr:', stderr)
+          })
+          .on('end', () => {
+            resolve("Video saved")
+          })
+      } else {
+        reject("error from readdir", err)
+      }
+    })
+  }
+  )
+}
+
+function p2vConverter(filepath, filename, videoOptions, pdf2PicOptions, videoPath, thumbnail, thumbnailPath) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const outputPath = path.join(__dirname, "/uploads/", `Folder_${filename}/`, `${filename}.pdf`)
+      const exportPath = path.join(__dirname, "/uploads/", `Folder_${filename}/`, "outputs")
+      const dir = path.join(__dirname, "/uploads/", `Folder_${filename}`)
+      fs.mkdir(exportPath, { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+      const readFile = fs.readFileSync(filepath);
+      await libreConverter(readFile, outputPath).then(libreMessage => console.log(libreMessage))
+      if (thumbnail) {
+        await generateThumbnail(outputPath, filename, thumbnailPath).then(thumbnailMessage => console.log(thumbnailMessage))
+      }
+      pdf2PicOptions.savedir = exportPath
+      pdf2PicOptions.savename = filename
+      await pdf2picConverter(outputPath, pdf2PicOptions).then(p2pMessage => console.log(p2pMessage))
+      await converttoVideo(exportPath, filename, videoOptions, videoPath).then(ctvMessage => {
+        return ctvMessage
+      })
+      fs.rmdir(dir, { recursive: true }, (err) => {
+        if (err) {
+          reject("error from rmdir", err)
+        }
+
+        console.log(`${dir} is deleted!`);
+      });
+      resolve(`${videoPath}/${filename}.mp4`)
+    } catch {
+      reject()
+    }
+  })
+}
+
+module.exports = { p2vConverter };
